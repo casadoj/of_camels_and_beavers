2d9032292880c12ec34d963cad91900abb92c215
casadoj <chus.casado.88@gmail.com>
2026-05-05 14:10:21 +0200

    #BEAVERS Add reservoirs in Andalucía

--- src/ocab/timeseries/utils.py
+++ src/ocab/timeseries/utils.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from datetime import datetime
+from typing import Dict, Optional
 
 
 def resample_daily(
@@ -8,7 +11,7 @@ def resample_daily(
     """Resample hourly time series to daily"""
 
     df = df.resample('D').mean().round(decimals)
-    df.index = df.index.date
+    df.index = df.index.normalize()
     df.index.name = 'date'
 
     return df
@@ -19,5 +22,31 @@ def compute_filling(df: pd.DataFrame, capacity: float) -> pd.DataFrame:
     
     if 'storage' in df.columns:
         df['filling'] = df['storage'] / capacity
+
+    return df
+
+
+def define_observed_period(
+        timeseries: Dict[int, pd.DataFrame], 
+        last_year: Optional[int] = None
+    ) -> (pd.DataFrame):
+    """Defines start and end year of the time series, and whether the station is active or not
+
+    Parameters:
+    -----------
+    timeseries: dictionary
+        A dictionary where keys are station IDs and values the observed time series
+    last_year: integer
+        Last recorded year. Used to define whether the station is active or not
+    """
+
+    if last_year is None:
+        last_year = datetime.now().year
+    df = pd.DataFrame(index=timeseries.keys(), columns=['start', 'end', 'active'], dtype='Int64')
+    for ID, ts in timeseries.items():
+        start, end = ts.index.min(), ts.index.max()
+        df.loc[ID, 'start'] = start.year
+        df.loc[ID, 'end'] = end.year if end.year < last_year else np.nan
+        df.loc[ID, 'active'] = 1 if end.year == last_year else 0
 
     return df
